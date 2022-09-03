7cd179eeef684556779fd76e31aa8822742fad8b
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-09-03 13:47:43 +0600

    some responsive

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ const displayLoadNews = catagorys => {
     catagorys.forEach(catagory => {
         // console.log(catagory)
         const creatDiv = document.createElement('div');
+        creatDiv.classList.add('col')
         creatDiv.innerHTML = `
         <p onclick="loadnewsDetails('${catagory.category_id}')">${catagory.category_name} </p>
         `;
@@ -51,32 +52,32 @@ const displayLoadDetais = items => {
         cardDiv.classList.add('row');
         cardDiv.classList.add('coustom-card');
         cardDiv.innerHTML = `
-        <div class="col-md-4">
-        <img src="${item.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        <div class="col-md-3">
+        <img src="${item.thumbnail_url}" class=" img-fluid rounded-start" alt="...">
       </div>
-      <div class="col-md-8">
+      <div class="col-md-9">
         <div class="card-body mt-4">
           <h5 class="card-title fs-4 mb-3 fw-semibold">${item.title}</h5>
           <p class="card-text">${item.details.slice(0, 400)} ...</p>
-          <div class="d-flex justify-content-between">
-                <div class="d-flex">
-                    <img src="${item.author.image_url}">
-                    <div class="ms-3 text-center">
+          <div class="d-flex justify-content-around pt-3">
+                <div class="d-flex align-items-center g-2">
+                    <img class="img-fluid person-img rounded-circle" src="${item.author.img}">
+                    <div class="ms-2 text-center text-nowrap">
                         <h6 class="fs-5">${item.author.name ? item.author.name : 'No Information'}</h6>
                         <p>${item.author.published_date ? item.author.published_date : 'No Information' }</p>
                     </div>
                 </div>
-                <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center me-5">
                     <i class="fa-solid fa-eye"></i>
                     <p class="ms-2 mt-3">${item.total_view ? item.total_view : '0'  }M</p>
                 </div>
-                <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center ms-5 me-5">
                 <i class="fa-solid fa-star-half-stroke"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 </div>
-                <div class="mt-3">
+                <div class="mt-3 ms-5 text-nowrap me-5">
                   <button onclick="loadCardItem('${item._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                   ->></button>
                 </div>
